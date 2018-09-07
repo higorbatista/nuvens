35bfea07624e43f2bca75eb92f9e1486f6c173d3
Thiago <thiagosousa8760@gmail.com>
2018-09-06 21:19:13 -0300

    Adicionando arquivo principal.js

---
+++ principal.js
@@ -0,0 +1,10 @@
+var banners =
+	["Os melhores do Brasil]!", "Qualidade e preço baixo!"]
+var bannerAtual = 0;
+
+function trocoBanner(){
+	bannerAtual = (bannerAtual + 1) % 2;
+	document.querySelector('h2#mensagem').textContent = banners[bannerAtual]
+}
+
+setInterval(trocoBanner, 2000);
